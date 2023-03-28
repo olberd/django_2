cacc9c758f0c0fb9cb21afe091a860ebde400266
Oleg <olberd@bk.ru>
2023-03-28 12:10:33 +0300

    Add blank start page with shortcuts.

--- where_to_go/views.py
+++ where_to_go/views.py
@@ -1,9 +1,6 @@
 from django.http import HttpResponse
-from django.template import loader
+from django.shortcuts import render
 
 
 def index(request):
-    template = loader.get_template('index.html')
-    context = {}
-    rendered_page = template.render(context, request)
-    return HttpResponse(rendered_page)
+    return render(request, 'index.html')
