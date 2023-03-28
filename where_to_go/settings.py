35f392b9a032dae97e81a75cd9f3e7c63a1a5f6c
Oleg <olberd@bk.ru>
2023-03-29 00:12:35 +0300

    Setup media files serving.

--- where_to_go/settings.py
+++ where_to_go/settings.py
@@ -127,6 +127,8 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
 
+MEDIA_ROOT = '/media/'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
