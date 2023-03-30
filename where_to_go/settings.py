b701098d947e4383b024c6ebb73a09c6cf010e1d
Oleg <olberd@bk.ru>
2023-03-30 21:22:27 +0300

    Move secret_key to .env.

--- where_to_go/settings.py
+++ where_to_go/settings.py
@@ -12,6 +12,11 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 import mimetypes
 import os
 from pathlib import Path
+from environs import Env
+
+
+env = Env()
+env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 from django.template.context_processors import media
@@ -23,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-mhn@5vsnu#*a8&kull#=)ovf66=g@_d%+nz=dc)tnh!wp6247)'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
