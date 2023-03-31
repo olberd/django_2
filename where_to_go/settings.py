c920f2f847a4b6d4abf0732dfe265d1dfc2a003b
Oleg <olberd@bk.ru>
2023-03-31 11:39:47 +0300

    Move secret_key to .env.

--- where_to_go/settings.py
+++ where_to_go/settings.py
@@ -9,14 +9,10 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-import mimetypes
 import os
 from pathlib import Path
 from environs import Env
 
-
-env = Env()
-env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 from django.template.context_processors import media
@@ -28,12 +24,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
+env = Env()
+env.read_env()
+
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env.bool('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS')
 
 
 # Application definition
@@ -144,4 +143,3 @@ MEDIA_ROOT = BASE_DIR / 'media'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-mimetypes.add_type("application/javascript", ".js", True)
