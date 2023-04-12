fbe5eee8d3811b2c85c41c69a288a9d567e3d3b1
Oleg <olberd@bk.ru>
2023-04-12 20:48:12 +0300

    Remove useless files.

--- places/views.py
+++ places/views.py
@@ -1,7 +1,6 @@
 from django.http import JsonResponse
 from django.shortcuts import render, get_object_or_404
 from django.urls import reverse
-
 from places.models import Place
 
 
@@ -9,29 +8,25 @@ def index(request):
     features = []
     places = Place.objects.all()
     for place in places:
-
         features_data = {
-                'type': 'Feature',
-                'geometry': {
-                    'type': 'Point',
-                    'coordinates': [place.lng, place.lat]
-                },
-                'properties': {
-                    'title': place.title,
-                    'detailsUrl': reverse('places', args=(place.id, )),
-                }
+            'type': 'Feature',
+            'geometry': {
+                'type': 'Point',
+                'coordinates': [place.lng, place.lat]
+            },
+            'properties': {
+                'title': place.title,
+                'detailsUrl': reverse('places', args=(place.id, )),
             }
+        }
         features.append(features_data)
-
     geo_places = {
         'type': 'FeatureCollection',
         'features': features
     }
-
     context = {
         'GeoJSON': geo_places,
     }
-
     return render(request, 'index.html', context=context)
 
 
@@ -43,15 +38,12 @@ def get_place_detail(request, place_id):
     description_short = place_details.description_short
     description_long = place_details.description_long
     coordinates = [place_details.lat, place_details.lng]
-
     place_descriptions = {
-
-            'title': title,
-            'imgs': imgs,
-            'description_short': description_short,
-            'description_long': description_long,
-            'coordinates': coordinates,
-
-            }
+        'title': title,
+        'imgs': imgs,
+        'description_short': description_short,
+        'description_long': description_long,
+        'coordinates': coordinates,
+    }
     return JsonResponse(place_descriptions, safe=False, json_dumps_params={'ensure_ascii': False, 'indent': 2})
 
