35916469ae1cf8443e63b317f8fd995f9cdc1f76
Oleg <olberd@bk.ru>
2023-04-12 20:50:31 +0300

    Fix models.py.

--- places/models.py
+++ places/models.py
@@ -3,8 +3,8 @@ from tinymce import models as tinymce_models
 
 
 class Place(models.Model):
-    title = models.CharField(max_length=200, blank=True)
-    description_short = models.TextField()
+    title = models.CharField(max_length=200)
+    description_short = models.TextField(blank=True)
     description_long = tinymce_models.HTMLField(blank=True)
     lng = models.FloatField()
     lat = models.FloatField()
